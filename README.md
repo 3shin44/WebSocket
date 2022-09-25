85829c9f7f6d02267a83788345a2d18e5c19fc5e
3shin44 <3shin44@gmail.com>
2022-09-25 23:09:31 +0800

    add readme

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# wsocket
+# WebSocket practice
 
-## Project setup
+## Introduction 說明
+Create local WebSocket server and client.  
+建立本地端WebSocket伺服器與用戶端
+
+## NPM install
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Launch WS server 伺服器啟動
 ```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
+node ws_server.js
 ```
 
-### Lints and fixes files
+### Launch WS client 用戶端啟動
 ```
-npm run lint
+npm run server
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+### Preview 預覽
+![server](./img/ws_server.png)
+![client](./img/ws_client.png)
+
+
+### References
+[JavaScript | WebSocket 讓前後端沒有距離](https://medium.com/enjoy-life-enjoy-coding/javascript-websocket-%E8%AE%93%E5%89%8D%E5%BE%8C%E7%AB%AF%E6%B2%92%E6%9C%89%E8%B7%9D%E9%9B%A2-34536c333e1b)  
+[How to read BLOB data from a WebSocket which is not an image](https://stackoverflow.com/questions/21338263/how-to-read-blob-data-from-a-websocket-which-is-not-an-image)  
+[MDN WebSocket](https://developer.mozilla.org/zh-TW/docs/Web/API/WebSocket)  
+[BLOB（Binary Large Object）](https://developer.mozilla.org/zh-TW/docs/Web/API/Blob)
--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,52 @@
     <div>
       <button @click="startConnect">啟動連線</button>
       <button @click="terminateConnect">終止連線</button>
+      <button @click="currentStatus">查看目前狀態</button>
     </div>
     <p>連線狀態: {{ connectStatus }}</p>
+    <p>readyState: {{(readyStateCode)}}</p>
     <div>
       <label for="">傳送訊息給伺服器 </label>
       <input type="text" v-model="inputData">
       <button @click="sendData">送出</button>
     </div>
     <p>接收訊息: {{ msg }}</p>
+  </div>
+
+  <div class="container">
+    <table>
+      <caption>readyState 代碼對照表</caption>
+      <thead>
+        <tr>
+          <th>常數</th>
+          <th>值</th>
+          <th>描述</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><code>CONNECTING</code></td>
+          <td><code>0</code></td>
+          <td>連線尚未打開。</td>
+        </tr>
+        <tr>
+          <td><code>OPEN</code></td>
+          <td><code>1</code></td>
+          <td>連線已打開，可以進行通訊。</td>
+        </tr>
+        <tr>
+          <td><code>CLOSING</code></td>
+          <td><code>2</code></td>
+          <td>連線正在進行關閉程序。</td>
+        </tr>
+        <tr>
+          <td><code>CLOSED</code></td>
+          <td><code>3</code></td>
+          <td>連線已關閉／連線不能打開。</td>
+        </tr>
+      </tbody>
+
+    </table>
   </div>
 
   <div class="container">
@@ -94,12 +132,20 @@
     <p>參考資料來源: </p>
     <ul>
       <li>
-        JavaScript | WebSocket 讓前後端沒有距離 <br>
+        <b>JavaScript | WebSocket 讓前後端沒有距離</b><br>
         https://medium.com/enjoy-life-enjoy-coding/javascript-websocket-%E8%AE%93%E5%89%8D%E5%BE%8C%E7%AB%AF%E6%B2%92%E6%9C%89%E8%B7%9D%E9%9B%A2-34536c333e1b
       </li>
       <li>
-        MDN WebSocket<br>
+        <b>How to read BLOB data from a WebSocket which is not an image</b><br>
+        https://stackoverflow.com/questions/21338263/how-to-read-blob-data-from-a-websocket-which-is-not-an-image
+      </li>
+      <li>
+        <b>MDN WebSocket</b><br>
         https://developer.mozilla.org/zh-TW/docs/Web/API/WebSocket
+      </li>
+      <li>
+        <b>BLOB（Binary Large Object）</b><br>
+        https://developer.mozilla.org/zh-TW/docs/Web/API/Blob
       </li>
     </ul>
   </div>
@@ -108,23 +154,130 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        connectStatus: "未啟動",
+        readyStateCode: "---",
+        msg: "等待中",
+        wsObj: null,
+        inputData: ""
+      }
+    },
+    methods: {
+      disConnect() {
+        return this.wsObj == null || this.wsObj.readyState != 1 ? true : false;
+      },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      // 啟動連線
+      startConnect() {
+        //使用 WebSocket 的網址向 Server 開啟連結
+        let ws = new WebSocket('ws://localhost:3000');
+
+
+        // 讓VUE其他函式可以存取WebSocket
+        this.wsObj = ws;
+
+        ws.onmessage = event => {
+          console.log(event);
+          if (event.data instanceof Blob) {
+            let reader = new FileReader();
+
+            reader.onload = () => {
+              console.log("Result: " + reader.result);
+              this.msg = reader.result;
+            };
+
+            reader.readAsText(event.data);
+          } else {
+            console.log("Result: " + event.data);
+          }
+        }
+
+        //開啟後執行的動作，指定一個 function 會在連結 WebSocket 後執行
+        ws.onopen = () => {
+          this.connectStatus = "已連線"
+        }
+
+        //關閉後執行的動作，指定一個 function 會在連結中斷後執行
+        ws.onclose = () => {
+          this.connectStatus = "連線已停止"
+        }
+      },
+
+      // 送出訊息
+      sendData() {
+        // 檢查是否連線，未連線則提示
+        console.log(this.disConnect());
+        if (this.disConnect()) {
+          this.msg = "(尚未連線!)";
+          return;
+        }
+
+        this.wsObj.send(String(this.inputData));
+      },
+
+      currentStatus() {
+        try {
+          this.wsObj.readyState == null ? this.readyStateCode = "未啟動" : this.readyStateCode = this.wsObj.readyState;
+        } catch (error) {
+          this.readyStateCode = "未啟動";
+        }
+
+      },
+
+      // 終止連線
+      terminateConnect() {
+        // 檢查是否連線，未連線則提示
+        if (this.disConnect) {
+          this.wsObj.close();
+        }
+
+
+      },
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  button {
+    margin: 0 8px 0 8px;
+
+  }
+
+  .container {
+    padding: 20px;
+  }
+
+  table{
+    margin: auto;
+  }
+
+  table,
+  th,
+  td {
+    border: 1px solid black;
+    text-align: left;
+  }
+
+  li {
+    padding-bottom: 10px;
+  }
+
+  .ref {
+    width: 50%;
+    margin: auto;
+    text-align: left;
+    word-wrap: break-word;
+  }
 </style>
